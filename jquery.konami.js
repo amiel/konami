206416cb02d593800403c8b72d3bf3bc6b832a44
Amiel Martin <amiel@carnesmedia.com>
2010-03-30 09:55:33 -0700

    add test, restructure jqueryness, and make readme better

--- jquery.konami.js
+++ jquery.konami.js
@@ -1,11 +1,15 @@
-(function() {
-        var cheatPos=0,cheat=[38,38,40,40,37,39,37,39,66,65];
-        $(document).bind('keydown',function(e) {
-                if (e.keyCode == cheat[cheatPos]) ++cheatPos; else cheatPos = 0;
-                if (cheatPos == cheat.length) $.event.trigger('konami');
-        });
+$.konami = (function() {
 
-        $.konami = function(fn) {
-                $(window).bind('konami', fn);
+    var cheatPos = 0,
+        konami = function(fn) {
+            $(window).bind('konami', fn);
         };
+        
+    konami.cheat = [38,38,40,40,37,39,37,39,66,65];
+    
+    $(document).bind('keydown',function(e) {
+        if (e.keyCode == konami.cheat[cheatPos]) ++cheatPos; else cheatPos = 0;
+        if (cheatPos == konami.cheat.length) $.event.trigger('konami');
+    });
+    return konami;
 })();
