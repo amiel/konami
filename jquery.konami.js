e66e15fa87c23b30db9d54bad709e4555e069ce0
Amiel Martin <amiel@carnesmedia.com>
2010-07-20 12:43:57 -0700

    add a comment about the default code

--- jquery.konami.js
+++ jquery.konami.js
@@ -1,15 +1,16 @@
 $.konami = (function() {
 
     var cheatPos = 0,
+        konami_event = 'konami',
         konami = function(fn) {
-            $(window).bind('konami', fn);
+            $(window).bind(konami_event, fn);
         };
         
     konami.cheat = [38,38,40,40,37,39,37,39,66,65];
     
     $(document).bind('keydown',function(e) {
         if (e.keyCode == konami.cheat[cheatPos]) ++cheatPos; else cheatPos = 0;
-        if (cheatPos == konami.cheat.length) $.event.trigger('konami');
+        if (cheatPos == konami.cheat.length) $.event.trigger(konami_event);
     });
     return konami;
 })();
